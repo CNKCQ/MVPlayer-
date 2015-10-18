20afb972719af1380f8255e03196652926d4985e
cnkcq <wangchengqvan@gmail.com>
2015-10-18 16:21:25 +0800

    注释掉了测试代码

--- MVPlayer/MVPlayer/UserInterface/Base(基类)/BaseViewController.m
+++ MVPlayer/MVPlayer/UserInterface/Base(基类)/BaseViewController.m
@@ -17,18 +17,18 @@
     [self chackNetwork];
 }
 - (void)chackNetwork{
-    if ([NetworkPort isInternetConnectionAvailable]) {
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"当前网络不可用,请检查网络连接!" preferredStyle:UIAlertControllerStyleAlert];
-        UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
- 
-        }];
-        action.enabled = YES;
-        [alert addAction:action];
-        [self presentViewController:alert animated:YES completion:nil];
-        
-    }
+//    if ([NetworkPort isInternetConnectionAvailable]) {
+//        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"当前网络不可用,请检查网络连接!" preferredStyle:UIAlertControllerStyleAlert];
+//        UIAlertAction *action = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+// 
+//        }];
+//        action.enabled = YES;
+//        [alert addAction:action];
+//        [self presentViewController:alert animated:YES completion:nil];
+//        
+//    }
     NSArray *array = @[@""];
-    debugLog(@"array----%@",array[1]);
+//    debugLog(@"array----%@",array[1]);
 }
 
 
